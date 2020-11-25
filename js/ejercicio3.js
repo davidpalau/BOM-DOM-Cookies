264c4959b82816b527a1b3509ac8b0c64b5e9003
davidpalau <palau.david@hotmail.com>
2020-11-25 19:27:27 +0100

    Update ejercicio3.js

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,1 +1,18 @@
+const palabras = ["Mayonesa","Manzana", "Banana", "Mañana", "Tarde", "Sacarina", "Index", "Desarrollador", "Clase", "Objeto"];
+palabras.forEach(palabras => document.write("<p>", palabras ,"</p>"));
 
+var preguntar = function(){window.confirm("¿Quieres ordenar las palabras.?")};
+
+var ordenar = window.setTimeout(preguntar, 3000);
+console.log(ordenar);
+
+if(ordenar){
+// A continuación se ordenan las palabras
+palabras.sort();
+mostrarPantalla(palabras);
+
+}
+
+function mostrarPantalla(lista) {
+    lista.forEach((elemento) => document.write(`<p>${elemento}</p>`));
+   }
