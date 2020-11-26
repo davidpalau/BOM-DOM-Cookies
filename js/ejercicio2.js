b8601bd30310a93e5abab129991d24ae91d6c409
David Palau Morant <palau.david@hotmail.com>
2020-11-26 13:32:08 +0100

    He repasado y actualizado los ejercicios

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,21 +1,24 @@
 // dias 28, horas 24, minutos 60, segundos 60 y milisegundos 1000, tiempo de caducidad.
 let finalCookie = 28 * 24 * 60 * 60 * 1000;
-
+//Consegir fecha de hoy.
 let fechaActual = new Date;
 
     fechaActual.setTime(fechaActual.getTime()+finalCookie);
 var contador=getCookie( "contador" );
+//Cuenta visitas
 ++contador;
-
+//Reinicio de de cuenta visitas 
 if(contador==11){
  eliminarCookies("contador", contador);
  document.writeln("Reiniciamos las visitas");
 }else{
     setCookie( "contador", contador);
+
+    //Informar al visitante el número de vistas 
     document.writeln( "Ha visitado usted esta pagina "+contador+" veces.");
 }
 
-
+// Cookie para poder guardar numero de visitas (Efecto base de datos.)
 console.log(document.cookie);
 function getCookie(name){
     var cname = name + "=";               
@@ -31,12 +34,12 @@ function getCookie(name){
     }
     return null;
   }
-
+// Cookie para poder guardar numero de visitas (Efecto base de datos.)
   function setCookie(name, value, expires) {
     document.cookie = name + "=" + value + 
     ((expires == null) ? "" : "; expires=" + expires.toGMTString());
   }
-
+//Borrar todas las cookies acunuladas
   function eliminarCookies(nombre, valor){
       for(i = valor; i > 0; i--){
         document.cookie = nombre + "=" + i+ "; expires=Thu, 01 Jan 1970 00:00:00 UTC";;
--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,18 +1,25 @@
-const palabras = ["Mayonesa","Manzana", "Banana", "Mañana", "Tarde", "Sacarina", "Index", "Desarrollador", "Clase", "Objeto"];
-palabras.forEach(palabras => document.write("<p>", palabras ,"</p>"));
+//Hasta que no este toda la pagina cargada no debe ejecutar inicio();
+//window.addEventListener("load", inicio); Este metodo si funciona correctamente en Firefox, pero no me funciona en Chrome
 
-var preguntar = function(){window.confirm("¿Quieres ordenar las palabras.?")};
 
-var ordenar = window.setTimeout(preguntar, 3000);
-console.log(ordenar);
+   
+//Pedir confirmación para ordenar 
+    function preguntar(){
+    let ordenar = window.confirm("Ordenar las palabras:");
+    if (ordenar) {
+        // En caso true ordenar y mostrar
+        console.log("Ordenar y mostrar.")
+        palabras.sort;
+        document.write("<p>Palabras ordenadas: </p>");
+        mostrar(palabras);
+    }
+    }
 
-if(ordenar){
-// A continuación se ordenan las palabras
-palabras.sort();
-mostrarPantalla(palabras);
-
+// Mostrar palabras de un array en HTML
+function mostrar(palabras){
+    palabras.forEach(indice = palabras => document.write("<p>", palabras, "</p>"));
 }
+const palabras = ["Mayonesa", "Manzana", "Banana", "Mañana", "Tarde", "Sacarina", "Index", "Desarrollador", "Clase", "Objeto"];
+mostrar(palabras);
 
-function mostrarPantalla(lista) {
-    lista.forEach((elemento) => document.write(`<p>${elemento}</p>`));
-   }
+window.setTimeout('preguntar()', 3000);
